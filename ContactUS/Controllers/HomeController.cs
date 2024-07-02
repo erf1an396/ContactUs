2ec4c270784842b80492f68fb553c733812d93a1
erf1an396 <em.karfarma@gmail.com>
2024-07-02 12:32:43 +0330

    Add project files.

---
+++ ContactUS/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using ContactUs.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContactUs.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SendMessage(Message message)
+        {
+            return Redirect("/home/Messages");
+        }
+    }
+}
---
+++ ContactUS/Models/Message.cs
@@ -0,0 +1,10 @@
+﻿namespace ContactUs.Models
+{
+    public class Message
+    {
+
+        public string username { get; set; }
+
+        public string password { get; set; }
+    }
+}
