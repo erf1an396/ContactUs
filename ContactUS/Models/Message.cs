93feecb0b3f85be0b9cf60813f9180950f703634
erf1an396 <em.karfarma@gmail.com>
2024-07-05 20:23:54 +0330

    taghelpers in message comment

--- ContactUS/Models/Message.cs
+++ ContactUS/Models/Message.cs
@@ -14,12 +14,23 @@ namespace ContactUs.Models
 
         [Display(Name = "شماره تماس")]
         [Required(ErrorMessage ="{0} را وارد کنید")]
+        [MaxLength(11,ErrorMessage ="novalid")]
+        [MinLength(11,ErrorMessage ="novalid")]
         public string PhoneNumber { get; set; }
 
         [Display(Name = "متن پیام")]
         [Required(ErrorMessage = "{0} را وارد کنید")]
         public string MessageBody { get; set; }
 
+
+        [Display(Name = "کد")]
         public int Code { get; set; }
+
+
+        //[Compare("PhoneNumber",ErrorMessage ="kkkkdkdkdkkd")] ****************
+        //[Range(10,29,ErrorMessage ="kkkkkkkkkkkk")] **************************
+        //[EmailAddress(ErrorMessage ="jiejijijfjeiji")]*************************
+
+
     }
 }
