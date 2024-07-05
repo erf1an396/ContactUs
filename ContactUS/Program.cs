1a01f4c31426b1bda2b1bd5b335e2b6d94a13c41
erf1an396 <em.karfarma@gmail.com>
2024-07-05 12:20:48 +0330

    enviroments and lunch setting

--- ContactUS/Program.cs
+++ ContactUS/Program.cs
@@ -9,13 +9,49 @@ builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+//Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+app.UseHsts();
 }
+
+
+static void ConfigureServices(IServiceCollection services)
+{
+
+    services.AddControllersWithViews();
+
+}
+
+static void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+{
+    var enviroment = env.EnvironmentName;
+    if (env.IsProduction())
+    {
+        app.Run(async (context) =>
+        {
+            await context.Response.WriteAsync(enviroment);
+        });
+    }
+    if (env.IsDevelopment())
+    {
+
+        app.Run(async (context) =>
+        {
+            await context.Response.WriteAsync(enviroment);
+        });
+
+    }
+    app.Run(async (context) =>
+    {
+        await context.Response.WriteAsync(enviroment);
+    });
+}
+
+
+
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
@@ -38,3 +74,6 @@ app.MapControllerRoute(
 
 app.Run();
 
+
+
+  
