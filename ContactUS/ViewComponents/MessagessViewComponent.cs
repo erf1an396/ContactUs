8c2ed31c88680f35fc54f7b5a7dd2aa9aaf64c73
erf1an396 <em.karfarma@gmail.com>
2024-07-06 17:01:32 +0330

    ViewComponent

---
+++ ContactUS/ViewComponents/MessagessViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ContactUs.ViewComponents
+{
+    public class MessagessViewComponent :ViewComponent
+    {
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return await Task.FromResult(View("TableMessage", DataBase.DataBase.Messages));
+        }
+    }
+}
